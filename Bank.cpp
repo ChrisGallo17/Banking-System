26712b617aa585bc4dd7720572d310b09353eb2c
Chris Gallo <cgallo@csustan.edu>
2018-10-08 12:46:11 -0700

    Structure for C++ banking system. Using Clion IDE

---
+++ Bank.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class bank
+{
+public:
+    void menu();
+    void addAccount();
+    void removeAccount();
+    void withdraw();
+    void deposit();
+
+};
+
+int main()
+{
+
+}
+
+void bank::menu()
+{
+
+}
+
+void bank::addAccount()
+{
+
+}
+
+void bank::removeAccount()
+{
+
+}
+
+void bank::withdraw()
+{
+
+}
+
+void bank::deposit()
+{
+
+}
