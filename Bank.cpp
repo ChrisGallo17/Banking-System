9f428b51a6b82eb7b367e7d02723ded7b186dd7f
Chris Gallo <cgallo@csustan.edu>
2018-10-08 15:43:05 -0700

    Added a menu interface

--- Bank.cpp
+++ Bank.cpp
@@ -3,7 +3,9 @@
 
 using namespace std;
 
-class bank
+int cursor;
+
+class Bank
 {
 public:
     void menu();
@@ -16,30 +18,77 @@ public:
 
 int main()
 {
+    Bank bk;
 
+    cout << "\nWelcome to your To-Do List" << endl;
+    bk.menu();
+
+    return 0;
 }
 
-void bank::menu()
+void Bank::menu()
+{
+    cout << "\nEnter 1 to create an account " << endl;
+    cout << "Enter 2 to view account balance " << endl;
+    cout << "Enter 3 to withdraw or deposit from account " << endl;
+    cout << "Enter 4 to remove account " << endl;
+    cout << "Enter 5 to exit " << endl;
+
+    cursor = 0;
+
+    cin >> cursor;
+
+    if (cursor == 1)
+    {
+        // Create account function
+        cout << "Create Account: Work in progress, enter another number" << endl;
+        menu();
+    }
+    else if(cursor == 2)
+    {
+        // View account balance
+        cout << "View Account: Work in progress, enter another number" << endl;
+        menu();
+    }
+    else if(cursor == 3)
+    {
+        // Withdraw or deposit function
+        cout << "Withdraw/Deposit: Work in progress, enter another number" << endl;
+        menu();
+    }
+    else if(cursor == 4)
+    {
+        // Remove account function
+        cout << "Remove account: Work in progress, enter another number" << endl;
+        menu();
+    }
+    else if(cursor == 5)
+    {
+        exit(5);
+    }
+    else
+    {
+        cout << "Please enter a valid option" << endl;
+        menu();
+    }
+}
+
+void Bank::addAccount()
 {
 
 }
 
-void bank::addAccount()
+void Bank::removeAccount()
 {
 
 }
 
-void bank::removeAccount()
+void Bank::withdraw()
 {
 
 }
 
-void bank::withdraw()
-{
-
-}
-
-void bank::deposit()
+void Bank::deposit()
 {
 
 }
